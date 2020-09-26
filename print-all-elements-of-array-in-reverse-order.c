6cb16f03f323e22e8c0f3dabe041b51450bd7485
Dev Prashant <52974592+Prashant-Bharaj@users.noreply.github.com>
2020-09-26 21:08:36 +0530

    just started

---
+++ print-all-elements-of-array-in-reverse-order.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int num, i=0;
+	scanf("%d", &num);
+	int arr[num];
+	for(i=0; i<num; i++){
+		scanf("%d", &arr[i]);
+	}
+	for(i=num-1;i>=0; i--){
+		printf("%d\n",arr[i]);
+	}
+}
